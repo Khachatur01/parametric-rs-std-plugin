34c4a1c140b951f2d3b9fedc88c6f8cf4d6bcd37
Khachatur Yeritsyan <khachatur.yeritsyan@russianschool.com>
2025-07-02 21:40:21 +0400

    Refactoring.

--- src/converter/model_geometry_converter.rs
+++ src/converter/model_geometry_converter.rs
@@ -1,9 +1,9 @@
 mod geometry;
 
 use crate::converter::model_geometry_converter::geometry::Geometry;
-use parametric_rs::model::{Model, ModelConversionError, ModelInto};
+use parametric_rs::model_3d::{Model3D, ModelConversionError, ModelInto};
 
-impl ModelInto<Geometry> for Model {
+impl ModelInto<Geometry> for Model3D {
     fn try_into(&self) -> Result<Vec<Geometry>, ModelConversionError> {
         let mut sketches = self.sketches.values().cloned().collect::<Vec<_>>();
         for feature in &self.features {
